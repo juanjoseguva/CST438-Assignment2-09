3d21c543d56e9f5cbc1629ffd8d541699a589292
Andrew Grant <59942239+androog3000@users.noreply.github.com>
2024-04-06 13:15:10 -0700

    Added imports from SectionController

--- src/main/java/com/cst438/controller/AssignmentController.java
+++ src/main/java/com/cst438/controller/AssignmentController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.cst438.dto.SectionDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
