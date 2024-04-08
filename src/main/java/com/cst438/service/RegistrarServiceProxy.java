b078f89bf642d048c94cd590aa917e476d27ac49
Jerry Do <kd65541@yahoo.com>
2024-04-08 16:40:19 -0700

    added switch case and removed @GeneratedValue, renamed action name in updateEnrollment

--- src/main/java/com/cst438/service/RegistrarServiceProxy.java
+++ src/main/java/com/cst438/service/RegistrarServiceProxy.java
@@ -1,17 +1,16 @@
 package com.cst438.service;
 
 import com.cst438.domain.*;
-import com.cst438.dto.AssignmentDTO;
-import com.cst438.dto.CourseDTO;
-import com.cst438.dto.EnrollmentDTO;
-import com.cst438.dto.GradeDTO;
+import com.cst438.dto.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 //TODO: file copied from RegistarServiceProxy
 // some items may need to be adjusted
@@ -37,30 +36,127 @@ public class RegistrarServiceProxy {
     @Autowired
     CourseRepository courseRepository;
 
+    @Autowired
+    SectionRepository sectionRepository;
+
+    @Autowired
+    TermRepository termRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
     @RabbitListener(queues = "gradebook_service")
     public void receiveFromRegistrar(String message)  {
         try {
             String[] messageParts = message.split(" ");
             String action = messageParts[0];
-            String dto = messageParts[1];
+            String dtoString = messageParts[1];
 
             //TODO: add remainder of actions
-            if (action == "updateGrade") {
-                EnrollmentDTO enrollmentDTO = fromJsonString(dto, EnrollmentDTO.class);
-                Enrollment e = enrollmentRepository.findEnrollmentByEnrollmentId(enrollmentDTO.enrollmentId());
-                if (e != null) {
-                    e.setGrade(enrollmentDTO.grade());
-                    enrollmentRepository.save(e);
-                }
-            } else if (action == "addCourse") {
-                CourseDTO courseDTO = fromJsonString(dto, CourseDTO.class);
-                Course c = new Course();
-                c.setTitle(courseDTO.title());
-                c.setCourseId(courseDTO.courseId());
-                c.setCredits(courseDTO.credits());
-                courseRepository.save(c);
+            switch(action){
 
+                case "addCourse":
+                    CourseDTO course = fromJsonString(dtoString, CourseDTO.class);
+                    Course c = new Course();
+                    c.setCredits(course.credits());
+                    c.setTitle(course.title());
+                    c.setCourseId(course.courseId());
+                    courseRepository.save(c);
+
+                    break;
+                case "updateCourse":
+                    course = fromJsonString(dtoString, CourseDTO.class);
+                    c = courseRepository.findById(course.courseId()).orElse(null);
+                    if (c==null) {
+                        throw  new ResponseStatusException( HttpStatus.NOT_FOUND, "course not found "+course.courseId());
+                    } else {
+                        c.setTitle(course.title());
+                        c.setCredits(course.credits());
+                        courseRepository.save(c);
+                    }
+
+                    break;
+                case "deleteCourse":
+                    course = fromJsonString(dtoString, CourseDTO.class);
+                    c = courseRepository.findById(course.courseId()).orElse(null);
+                    // if course does not exist, do nothing.
+                    if (c!=null) {
+                        CourseDTO courseDTO = new CourseDTO(
+                                c.getCourseId(),
+                                c.getTitle(),
+                                c.getCredits()
+                        );
+                    }
+                    courseRepository.delete(c);
+
+                    break;
+                case "addSection":
+                    SectionDTO section = fromJsonString(dtoString, SectionDTO.class);
+                    c = courseRepository.findById(section.courseId()).orElse(null);
+                    if (c == null ){
+                        throw  new ResponseStatusException( HttpStatus.NOT_FOUND, "course not found "+section.courseId());
+                    }
+                    Section s = new Section();
+                    s.setCourse(c);
+
+                    Term term = termRepository.findByYearAndSemester(section.year(), section.semester());
+                    if (term == null) {
+                        throw  new ResponseStatusException( HttpStatus.NOT_FOUND, "year, semester invalid ");
+                    }
+                    s.setTerm(term);
+
+                    s.setSecId(section.secId());
+                    s.setBuilding(section.building());
+                    s.setRoom(section.room());
+                    s.setTimes(section.times());
+
+                    User instructor = null;
+                    if (section.instructorEmail()==null || section.instructorEmail().equals("")) {
+                        s.setInstructor_email("");
+                    } else {
+                        instructor = userRepository.findByEmail(section.instructorEmail());
+                        if (instructor == null || !instructor.getType().equals("INSTRUCTOR")) {
+                            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "email not found or not an instructor " + section.instructorEmail());
+                        }
+                        s.setInstructor_email(section.instructorEmail());
+                    }
+                    sectionRepository.save(s);
+
+                    break;
+                case "updateSection":
+                    section = fromJsonString(dtoString, SectionDTO.class);
+                    s = sectionRepository.findById(section.secNo()).orElse(null);
+                    if (s==null) {
+                        throw  new ResponseStatusException( HttpStatus.NOT_FOUND, "section not found "+section.secNo());
+                    }
+                    s.setSecId(section.secId());
+                    s.setBuilding(section.building());
+                    s.setRoom(section.room());
+                    s.setTimes(section.times());
+
+                    instructor = null;
+                    if (section.instructorEmail()==null || section.instructorEmail().equals("")) {
+                        s.setInstructor_email("");
+                    } else {
+                        instructor = userRepository.findByEmail(section.instructorEmail());
+                        if (instructor == null || !instructor.getType().equals("INSTRUCTOR")) {
+                            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "email not found or not an instructor " + section.instructorEmail());
+                        }
+                        s.setInstructor_email(section.instructorEmail());
+                    }
+                    sectionRepository.save(s);
+
+                    break;
+                case "deleteSection":
+                    section = fromJsonString(dtoString, SectionDTO.class);
+                    s = sectionRepository.findById(section.secNo()).orElse(null);
+                    if (s != null) {
+                        sectionRepository.delete(s);
+                    }
+
+                    break;
             }
+
         } catch (Exception e){
             System.out.println("Exception in receiveFromRegistrar "+ e.getMessage());
         }
@@ -68,7 +164,7 @@ public class RegistrarServiceProxy {
 
     //enrollment methods
     public void updateEnrollment(EnrollmentDTO enrollmentDTO){
-        sendMessage("updateEnrollment " + asJsonString(enrollmentDTO));
+        sendMessage("updateGrade" + asJsonString(enrollmentDTO));
     }
 
     //the following methods correspond to controller methods but may not be necessary
