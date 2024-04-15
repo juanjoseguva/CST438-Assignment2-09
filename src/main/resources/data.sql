c72a5f2b6ed20b89e8c5602658642d39de5fd99a
Andrew Grant <59942239+androog3000@users.noreply.github.com>
2024-04-14 19:09:14 -0700

    Update data.sql
    Added additional course information for testing

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,7 @@ insert into term (term_id, tyear, semester, add_date, add_deadline, drop_deadlin
 (7, 2023, 'Spring', '2022-11-01', '2023-01-30', '2023-01-30', '2023-01-15', '2023-05-17'),
 (8, 2023, 'Fall' ,  '2023-05-01', '2023-08-30', '2023-08-30', '2023-08-15', '2023-12-17'),
 (9, 2024, 'Spring', '2023-11-01', '2024-04-30', '2024-03-30', '2024-01-15', '2024-05-17'),
-(10, 2024, 'Fall',  '2024-05-01', '2024-08-30', '2024-08-30', '2024-08-15', '2024-12-17');
+(10, 2024, 'Fall',  '2024-04-01', '2024-06-12', '2024-08-30', '2024-08-15', '2024-12-17');
 
 insert into user_table (id, name, email, password, type) values
 (1, 'david wisneski', 'dwisneski@csumb.edu', '$2a$10$fRWtuq3dw6g3bhPWeHnsNOZ5slcDc38Xj.urfYIY2XVwFaFu/hVBK', 'INSTRUCTOR'),
@@ -23,7 +23,8 @@ insert into course values
 ('cst334', 'Operating Systems', 4),
 ('cst363', 'Introduction to Database', 4),
 ('cst438', 'Software Engineering', 4),
-('cst499', 'Capstone', 4);
+('cst499', 'Capstone', 4),
+('cst420', 'Herbology', 4);
 
 insert into section(section_no, course_id, sec_id, term_id, building, room, times, instructor_email ) values
 (1, 'cst338', 1, 8, '052', '100', 'M W 10:00-11:50', 'jgross@csumb.edu'),
@@ -35,7 +36,8 @@ insert into section(section_no, course_id, sec_id, term_id, building, room, time
 (7, 'cst338', 2, 9, '052', '100', 'M W 10:00-11:50', 'jgross@csumb.edu'),
 (8, 'cst363', 1, 9, '052', '104', 'M W 10:00-11:50', 'dwisneski@csumb.edu'),
 (9, 'cst363', 2, 9, '052', '102', 'M W 2:00-3:50', 'dwisneski@csumb.edu'),
-(10, 'cst438', 1, 9, '052', '222', 'T Th 12:00-1:50', 'dwisneski@csumb.edu');
+(10, 'cst438', 1, 9, '052', '222', 'T Th 12:00-1:50', 'dwisneski@csumb.edu'),
+(11, 'cst420', 1, 10, '052', '222', 'M W F 4:20-4:30', 'dwisneski@csumb.edu');
 
 insert into enrollment (enrollment_id, grade, section_no, user_id ) values
 (1, 'A', 1, 3),
